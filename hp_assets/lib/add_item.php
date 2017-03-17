b0444aea66beb3263d49d2ceac3ab9ed2f30087c
calvinmclean <calvinmclean@email.arizona.edu>
2017-03-17 13:55:13 -0700

    add page and php for modifying config, link page in config

---
+++ hp_assets/lib/add_item.php
@@ -0,0 +1,45 @@
+<?php
+
+$config = json_decode(file_get_contents('../../config.json'), true);
+
+if( isset($_POST['alt']) && isset($_POST['icon']) && isset($_POST['link']) ) {
+  $alt = $_POST['alt'];
+  $icon = $_POST['icon'];
+  $link = $_POST['link'];
+
+  $i = 0;
+
+  // Iterate through json to iterate $i
+  foreach ( $config['items'] as $key=>$entry ) $i++;
+  $config['items'][$i]['alt'] = $alt;
+  $config['items'][$i]['icon'] = $icon;
+  $config['items'][$i]['link'] = $link;
+}
+
+if ( isset($_POST['title']) && $_POST['title'] != "" )
+  $config['title'] = $_POST['title'];
+
+if ( isset($_POST['unlock_pattern']) && $_POST['unlock_pattern'] != "" )
+  $config['unlock_pattern'] = $_POST['unlock_pattern'];
+
+if ( isset($_POST['clock_format']) && $_POST['clock_format'] != "" )
+  $config['clock_format'] = $_POST['clock_format'];
+
+if ( isset($_POST['custom_url']) && $_POST['custom_url'] != "" )
+  $config['custom_url'] = $_POST['custom_url'];
+
+if ( isset($_POST['custom_url_selector']) && $_POST['custom_url_selector'] != "" )
+  $config['custom_url_selector'] = $_POST['custom_url_selector'];
+
+if ( isset($_POST['custom_url_headers']) && $_POST['custom_url_headers'] != "" )
+  $config['custom_url_headers'] = $_POST['custom_url_headers'];
+
+if ( isset($_POST['unsplash_client_id']) && $_POST['unsplash_client_id'] != "" )
+  $config['unsplash_client_id'] = $_POST['unsplash_client_id'];
+
+if ( isset($_POST['idle_timer']) && $_POST['idle_timer'] != "" )
+  $config['idle_timer'] = $_POST['idle_timer'];
+
+$new_config = json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+file_put_contents('../../config.json', $new_config);
+?>
